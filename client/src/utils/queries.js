4d1c9ce27d2618d9f335a0d8eae9be2762c07925
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-29 18:57:44 -0400

    Added queries and mutation to the client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,7 @@
-// client/src/utils/queries.js
 import { gql } from '@apollo/client';
 
 export const GET_USERS = gql`
-  query getUsers {
+  query GetUsers {
     users {
       id
       username
@@ -13,24 +12,18 @@ export const GET_USERS = gql`
 `;
 
 export const GET_USER = gql`
-  query getUser($id: ID!) {
+  query GetUser($id: ID!) {
     user(id: $id) {
       id
       username
       email
       role
-      products {
-        id
-        name
-        description
-        price
-      }
     }
   }
 `;
 
 export const GET_PRODUCTS = gql`
-  query getProducts {
+  query GetProducts {
     products {
       id
       name
@@ -39,13 +32,15 @@ export const GET_PRODUCTS = gql`
       artisan {
         id
         username
+        email
+        role
       }
     }
   }
 `;
 
 export const GET_PRODUCT = gql`
-  query getProduct($id: ID!) {
+  query GetProduct($id: ID!) {
     product(id: $id) {
       id
       name
@@ -54,7 +49,91 @@ export const GET_PRODUCT = gql`
       artisan {
         id
         username
+        email
+        role
       }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      userId
+      token
+      tokenExpiration
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!, $role: String!) {
+    addUser(username: $username, email: $email, password: $password, role: $role) {
+      id
+      username
+      email
+      role
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: ID!, $username: String!, $email: String!, $role: String!) {
+    updateUser(id: $id, username: $username, email: $email, role: $role) {
+      id
+      username
+      email
+      role
+    }
+  }
+`;
+
+export const ADD_PRODUCT = gql`
+  mutation AddProduct($name: String!, $description: String!, $price: Float!, $artisanId: ID!) {
+    addProduct(name: $name, description: $description, price: $price, artisanId: $artisanId) {
+      id
+      name
+      description
+      price
+      artisan {
+        id
+        username
+        email
+        role
+      }
+    }
+  }
+`;
+
+export const UPDATE_PRODUCT = gql`
+  mutation UpdateProduct($id: ID!, $name: String!, $description: String!, $price: Float!) {
+    updateProduct(id: $id, name: $name, description: $description, price: $price) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      id
+      username
+      email
+      role
+    }
+  }
+`;
+
+export const DELETE_PRODUCT = gql`
+  mutation DeleteProduct($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+      description
+      price
     }
   }
 `;
