9a0231f61d1011150f1e34e4c79d38adbf77d5f8
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-04-01 19:47:04 -0400

    Added photos to the product models

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -7,48 +7,49 @@ enum Role {
   BUYER
 }
 
-  type User {
-    id: ID!
-    username: String!
-    email: String!
-    role: Role!
-    products: [Product] # For artisans to see their products
-  }
+type User {
+  id: ID!
+  username: String!
+  email: String!
+  role: Role!
+  products: [Product] # For artisans to see their products
+}
 
-  type Product {
-    id: ID!
-    name: String!
-    description: String!
-    price: Float!
-    artisan: User
-    createdAt: String
-    updatedAt: String
-  }
+type Product {
+  id: ID!
+  name: String!
+  description: String!
+  price: Float!
+  artisan: User
+  imageURL: String! # Added field for the image URL
+  createdAt: String
+  updatedAt: String
+}
 
-  type AuthData {
-    userId: ID!
-    token: String!
-    tokenExpiration: Int!
-  }
+type AuthData {
+  userId: ID!
+  token: String!
+  tokenExpiration: Int!
+}
 
-  # Query type for fetching data
-  type Query {
-    users: [User]
-    user(id: ID!): User
-    products: [Product]
-    product(id: ID!): Product
-    login(email: String!, password: String!): AuthData
-  }
+# Query type for fetching data
+type Query {
+  users: [User]
+  user(id: ID!): User
+  products: [Product]
+  product(id: ID!): Product
+  login(email: String!, password: String!): AuthData
+}
 
-  # Mutation type for data modification
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!, role: Role!): User
-    addProduct(name: String!, description: String!, price: Float!, artisanId: ID!): Product
-    updateUser(id: ID!, username: String, email: String, role: Role): User
-    updateProduct(id: ID!, name: String, description: String, price: Float): Product
-    deleteUser(id: ID!): User
-    deleteProduct(id: ID!): Product
-  }
+# Mutation type for data modification
+type Mutation {
+  addUser(username: String!, email: String!, password: String!, role: Role!): User
+  addProduct(name: String!, description: String!, price: Float!, artisanId: ID!, imageURL: String!): Product # Updated mutation to include imageURL
+  updateUser(id: ID!, username: String, email: String, role: Role): User
+  updateProduct(id: ID!, name: String, description: String, price: Float, imageURL: String): Product # Updated mutation to optionally update imageURL
+  deleteUser(id: ID!): User
+  deleteProduct(id: ID!): Product
+}
 `;
 
 module.exports = typeDefs;
