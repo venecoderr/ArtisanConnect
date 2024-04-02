0cdfa50ab9098d31e24864818c28e8fbc287f88f
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-02 19:17:36 -0400

    queries context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { Outlet } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { QueriesProvider } from './utils/QueriesContext.jsx';
 import Nav from "./components/Nav";
-// import Form from './components/form';
-import { FormProvider } from './utils/FormContext';
+import Form from './components/form';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql'
@@ -18,11 +18,11 @@ export default function App(){
   return(
     <div className="w-full h-screen bg-gradient-to-r from-amber-100 to-stone-300/70">
       <ApolloProvider client={client}>
-        <Nav/>
-        <Outlet/>
-        <FormProvider>
-          {/* <Form/> */}
-        </FormProvider>
+        <QueriesProvider>
+          <Nav/>
+          {/* <Outlet/> */}
+          <Form/>
+        </QueriesProvider>
       </ApolloProvider>
     </div>
   )
