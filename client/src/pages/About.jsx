4f9abb5a548df92b675c660689495052f585ce37
pamcakesplease <pamelapimentel8@gmail.com>
2024-04-02 15:07:22 -0400

    updated about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import SignUpForm from '../components/ui/signUpForm.jsx'
 
 export default function About(){
     return(
         <>
-            <SignUpForm/>
+     <div id='main'>
+            <img className="about-bg-cover right-0" src="/assets/cover-2.png"></img>
+            <h1 className="about-title">About Us</h1>
+            <div id="about">
+            Welcome to Artisan Connect! ArtisanConnect is a vibrant, community-driven marketplace designed to bridge the gap between local artisans and enthusiasts of handmade, unique products.
+            Our platform empowers local creators by providing them with a digital space to showcase and sell their creations, ranging from handmade jewelry and crafts to bespoke furniture and art. 
+            By fostering direct connections between artisans and buyers, ArtisanConnect not only supports the growth of local economies but also promotes sustainable consumer practices. 
+            Our intuitive, user-friendly interface ensures an enjoyable browsing and shopping experience, enhanced with secure payment options, artist portfolios, and real-time communication tools.
+            </div>
+            </div>    
         </>
-    )
-}
+    );
+};
+        
