1953aa68071a5511422a45cdb8cef8079e3df57a
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-01 22:41:11 -0400

    forms and context

--- client/src/components/ui/signUpForm.jsx
+++ client/src/components/ui/signUpForm.jsx
@@ -1,177 +1,146 @@
 import { useState } from "react";
-import { classNames } from "../../utils/utils.js";
 import RequiredField from "./requiredField.jsx";
 
-export default function SignUpForm(){
-    //States
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState({address: '', valid: true})
-    const [message, setMessage] = useState('')
+export default function SignUpForm({ addUser, classNames, validateEmail }) {
+
     const [form, setForm] = useState({
-      sender: '',
-      email: '',
-      message: '',
-      sentOn: ''
-    })
+        username: '',
+        email: { address: '', valid: true },
+        password: ''
+    });
+
     const [showRequired, setShowRequired] = useState({
-        firstName: false,
-        lastName: false,
+        username: false,
         email: false,
-        message: false
-    })
-    const [errorMessage, setErrorMessage] = useState('')
+        password: false
+    });
+    
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
-        
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'first-name') {
-          setFirstName(inputValue);
-        } else if (inputType === 'last-name') {
-          setLastName(inputValue);
-        } else if (inputType === 'email') {
-          const validEmail = validateEmail(inputValue)
-          setEmail({ address: inputValue, valid: validEmail});
-          console.log(email)
-        } else {
-          setMessage(inputValue);
-        }
-    };
-    
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        e.preventDefault();
-    
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email)) {
-          setErrorMessage('Invalid e-mail');
-          return;
-        }else if(!firstName || !lastName){
-          setErrorMessage('Missing name');
-          return;
-        }else if(!message){
-          setErrorMessage('Missing message');
-          return;
-        }
-    
+        const { name, value } = e.target;
         setForm({
-          sender: `${firstName} ${lastName}`,
-          email: email,
-          message: message
-        })
-    
-        console.log(form)
-    
-        setFirstName('');
-        setLastName('');
-        setEmail({address: '', valid: true});
-        setMessage('');
+            ...form,
+            [name]: name === 'email' ? { address: value, valid: validateEmail(value) } : value
+        });
+        setShowRequired({
+            ...showRequired,
+            [name]: !value
+        });
     };
 
-   return (
-    <>
-        <form className="form" onSubmit={handleFormSubmit}>
-            <section className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <section>
-                    <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                    First name
-                    </label>
-                    <section className="mt-2.5">
-                    <input
-                    value={firstName}
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    onBlur={() => { 
-                        !firstName ? setShowRequired({...showRequired, firstName: true}) : setShowRequired({...showRequired, firstName: false})
-                        }}
-                    onChange={handleInputChange}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6"
-                    />
-                    {showRequired.firstName && <RequiredField/>}
-                </section>
-                </section>
-                <section>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                    Last name
-                </label>
-                <section className="mt-2.5">
-                    <input
-                    value={lastName}
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    onBlur={() => { 
-                        !lastName ? setShowRequired({...showRequired, lastName: true}) : setShowRequired({...showRequired, lastName: false})
-                    }}
-                    onChange={handleInputChange}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6"
-                    />
-                    {showRequired.lastName && <RequiredField/>}
-                </section>
-                </section>
-                <section className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                    Email
-                </label>
-                <section className="mt-2.5">
-                    <input
-                    value={email.address}
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    onBlur={() => { 
-                        !email.address ? setShowRequired({...showRequired, email: true}) : setShowRequired({...showRequired, email: false})
-                    }}
-                    onChange={handleInputChange}
-                    className={classNames(!email.valid ? 'border-2 border-red-700' : '', "block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6")}
-                    />
-                    {!email.valid && (<p className='text-red-700'>Invalid e-mail</p>)}
-                    {showRequired.email && <RequiredField/>}
-                </section>
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
 
+        if (!validateEmail(form.email.address)) {
+            setErrorMessage('Invalid email');
+            return;
+        } else if (!form.username) {
+            setErrorMessage('Username is required');
+            return;
+        } else if (!form.password) {
+            setErrorMessage('Password is required');
+            return;
+        }
+
+        try {
+            // Call addUser mutation function with the form data
+            const newUser = await addUser({
+                variables: {
+                    username: form.username,
+                    email: form.email.address,
+                    password: form.password
+                }
+            });
+
+            console.log(newUser)
+            // Reset form state
+            setForm({
+                username: '',
+                email: { address: '', valid: true },
+                password: ''
+            });
+            setShowRequired({
+                username: false,
+                email: false,
+                password: false
+            });
+            setErrorMessage('');
+        } catch (error) {
+            console.error('Error occurred during form submission:', error);
+            setErrorMessage('An error occurred while processing your request');
+        }
+    };
+
+    return (
+        <>
+            <form className="form" onSubmit={handleFormSubmit}>
+                <section className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <section>
+                        <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Username
+                        </label>
+                        <section className="mt-2.5">
+                            <input
+                                value={form.username}
+                                type="text"
+                                name="username"
+                                id="username"
+                                onMouseLeave={() => setShowRequired({ ...showRequired, username: !form.username })}
+                                onChange={handleInputChange}
+                                className={classNames(showRequired.username && !form.username && 'border-2 border-red-700', "block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6")}
+                            />
+                            {showRequired.username && !form.username && <RequiredField />}
+                        </section>
+                    </section>
+                    <section>
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Email
+                        </label>
+                        <section className="mt-2.5">
+                            <input
+                                value={form.email.address}
+                                type="email"
+                                name="email"
+                                id="email"
+                                onMouseLeave={() => setShowRequired({ ...showRequired, email: !form.email.address })}
+                                onChange={handleInputChange}
+                                className={classNames(!form.email.valid && 'border-2 border-red-700', "block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6")}
+                            />
+                            {!form.email.valid && <p className='text-red-700'>Invalid email</p>}
+                            {showRequired.email && !form.email.address && <RequiredField />}
+                        </section>
+                    </section>
+                    <section className="sm:col-span-2">
+                        <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Password
+                        </label>
+                        <section className="mt-2.5">
+                            <input
+                                value={form.password}
+                                type="password"
+                                name="password"
+                                id="password"
+                                onMouseLeave={() => setShowRequired({ ...showRequired, password: !form.password })}
+                                onChange={handleInputChange}
+                                className={classNames(showRequired.password && !form.password && 'border-2 border-red-700', "block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6")}
+                            />
+                            {showRequired.password && !form.password && <RequiredField />}
+                        </section>
+                    </section>
+                    <section className="mt-10 sm:col-span-2">
+                        <button
+                            type="submit"
+                            className="block p-3.5 text-center text-sm text-grey-900 font-semibold shadow-sm"
+                        >SignUp</button>
+                    </section>
                 </section>
-                <section className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-                    Message
-                </label>
-                <section className="mt-2.5">
-                    <textarea
-                    name="message"
-                    id="message"
-                    rows={4}
-                    onBlur={() => { 
-                        !message ? setShowRequired({...showRequired, message: true}) : setShowRequired({...showRequired, message: false})
-                    }}
-                    onChange={handleInputChange}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-700 sm:text-sm sm:leading-6"
-                    defaultValue={''}
-                    />
-                    {showRequired.message && <RequiredField/>}
-                </section>
-                </section>
-                <section className="mt-10 sm:col-span-2">
-                <button
-                    type="submit"
-                    className="block w-full rounded-md accent-bg px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:main-bg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700"
-                >
-                    Let's talk
-                </button>
-                </section>
-            </section>
             </form>
             {errorMessage && (
-            <section className="m-3">
-                <p className="error-text">{errorMessage}</p>
-            </section>
+                <section className="m-3">
+                    <p className="error-text">{errorMessage}</p>
+                </section>
             )}
         </>
-   ) 
+    )
 }
