a84cf70c4cfa5c3a97650b1127522443434ca9b7
maferadr <maferadr0604@gmail.com>
2024-04-02 18:01:44 -0400

    Working on products.jsx

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import SignUpForm from '../components/ui/signUpForm.jsx'
 
+//let him know that signUpForm throws up an error
+const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+  ]
 
 export default function Products(){
     return(
         <>
-            <SignUpForm/>
+            <div className="img-container">
+                <img className="img-products" src="/assets/products-cover.jpg"></img>
+            </div>
+
+            {/* Products container */}
+            
         </>
     )
 }
