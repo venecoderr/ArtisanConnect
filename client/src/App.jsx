884f931439cbd5fc776d79f0cbf9a38a86ebbfdb
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-02 21:12:04 -0400

    product form done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ export default function App(){
         <QueriesProvider>
           <Nav/>
           {/* <Outlet/> */}
-          <Form formType={'product'}/>
+          <Form formType={'signup'}/>
         </QueriesProvider>
       </ApolloProvider>
     </div>
