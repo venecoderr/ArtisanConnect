bc4cf8316342b1271a46a0e230ecb22fc30d2195
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-04 19:01:48 -0400

    bugfix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,12 +17,13 @@ export default function Profile() {
     console.log(userProducts); // Check the structure of userProducts
 
     return (
-      <>
-        <img className="element-cover right-0" src="/assets/cover-2.png"></img>
-        <div className="w-full h-screen bg-gradient-to-r from-stone-300/70 to-amber-100">
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-4 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="h-screen">
+        <div className="w-full h-100 bg-gradient-to-r from-stone-300/70 to-amber-100">
+        <img className="cover-dashboard object-cover" src="/assets/dashboard-cover.jpg"></img>
+          <div className="dashboard-layout mx-auto max-w-2xl p-4 justify-center">
             <p className="username">Hi, {user.username}</p>
-            <img src='https://placehold.co/400' alt='profile picture' className="user-picture"/>
+            <p className="user-description">Thanks for supporting our Community!</p>
+            {/* <img src='https://placehold.co/400' alt='profile picture' className="user-picture"/> */}
             <p className="user-products">Your Products</p>
             <div id="products-card" className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
               {userProducts.map((product) => (
@@ -51,6 +52,6 @@ export default function Profile() {
             </div>
           </div>
         </div>
-      </>
+      </div>
     );
   }
