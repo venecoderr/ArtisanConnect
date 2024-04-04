55438dcf9064045732e89c93fc6935abc92fdd93
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-04-04 19:55:38 -0400

    Added bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // You can adjust the number of rounds as needed
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -15,6 +17,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+});
+
+// Pre-save hook to hash the password
+userSchema.pre('save', function(next) {
+  // Check if the password has been modified (or is new)
+  if (!this.isModified('password')) return next();
+
+  // Generate a salt and hash the password
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // Replace the plain password with the hashed one
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
