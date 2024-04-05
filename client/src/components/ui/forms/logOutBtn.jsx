9667e6c7cbbbb9fe2d629df7de4f7abf7702332c
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-04 20:07:58 -0400

    logout modified

--- client/src/components/ui/forms/logOutBtn.jsx
+++ client/src/components/ui/forms/logOutBtn.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import auth from "../../../utils/auth"
 
 export default function LogOutBtn() {
 
-    const [ display, setDisplay ] = useState(false)
+    const [ display, setDisplay ] = useState(auth.loggedIn())
+    
     return (
     <>
         { display && <p className='text-red-700'>LOGOUT</p> }
