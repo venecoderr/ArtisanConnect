56ec7f95c81bc08b79d70641d4a48955262520c5
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-04-04 21:02:03 -0400

    Back to the working

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
+
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -19,26 +18,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Pre-save hook to hash the password
-userSchema.pre('save', function(next) {
-  var user = this;
-
-  // Only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // Generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // Hash the password using the generated salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      // Override the plaintext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
 
 module.exports = mongoose.model('User', userSchema);
