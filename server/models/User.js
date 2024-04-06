977434017856e9aba215e7bceca3f75519b1190f
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-04-06 09:59:26 -0400

    removed hash from resolvers again

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -17,6 +18,26 @@ const userSchema = new mongoose.Schema({
     required: true
   },
 });
+// Pre-save hook to hash the password
+userSchema.pre('save', function(next) {
+  var user = this;
 
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // Hash the password using the generated salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // Override the plaintext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 module.exports = mongoose.model('User', userSchema);
