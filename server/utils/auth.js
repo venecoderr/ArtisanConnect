ccd59e58832278b75e3adcc3f4cf6f8fa2eb032b
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-04-06 20:29:52 -0400

    matched token expiration time

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,9 +26,9 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
+    } catch (error) {
+      console.log('Invalid token:', error.message);
+    }    
 
     return req;
   },
