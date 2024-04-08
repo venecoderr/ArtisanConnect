5fa15808177dceba925296088ed485c3d6de793c
maferadr <maferadr0604@gmail.com>
2024-04-08 10:15:38 -0400

    testing animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom";
 import { QueriesProvider } from './utils/QueriesContext.jsx';
 import Nav from "./components/Nav";
 
+
 export default function App(){
   return(
-    <div className="w-full h-100 bg-gradient-to-r from-amber-100 to-stone-300/70">
+    <div 
+    className="main-div w-full h-100 bg-gradient-to-r from-amber-100 to-stone-300/70"
+    >
 
         <QueriesProvider>
           <Nav/>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export default function Home(){
     return(
@@ -9,11 +10,17 @@ export default function Home(){
                 <div className='img-container absolute left-0 pr-40'>
                     <img className="object-cover w-100 h-screen" src='/assets/Artisan.png'></img>
                 </div>
-                <div id="main-text">
+                <motion.div id="main-text"
+                animate={{
+                    scale: 0.8,
+                    transition: {
+                        duration: 1,
+                    }
+                }}>
                     <h1 className="text">Artisan
                     <br></br>
                     <span className="connect">Connect</span></h1>
-                </div>
+                </motion.div>
                 <div className="absolute button">
                 <Link to ="/signup">
                 <button className="bg-transparent text-brown-700 font-semibold py-2 px-4 border rounded">
